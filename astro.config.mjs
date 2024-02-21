e88f97e64edf96d54d8c98dde7f81b841f01a24f
Revati Tambe <24revati@gmail.com>
2024-02-20 22:35:07 -0800

    testing w/pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://djarty24.github.io/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
