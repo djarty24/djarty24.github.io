c282846cedbc930540cb82dd7311666e44fa3cc5
Revati Tambe <24revati@gmail.com>
2024-11-19 18:57:38 -0800

    help

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-    base: '/djarty24.github.io/'
-})
+    base: '/',
+    build: {
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+        },
+      },
+    },
+});  
